01b8cbd039c83f6f94f13796de3cf562ccff4540
Annisa Eva <annisa.eat@gmail.com>
2021-07-24 02:50:18 +0700

    memanfaatkan widget SafeArea.

--- aplikasi_tempat_wisata/lib/main.dart
+++ aplikasi_tempat_wisata/lib/main.dart
@@ -16,14 +16,17 @@ class MyApp extends StatelessWidget {
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-    body:
-    Column(
-      children: <Widget>[
-        Container(
-            margin: EdgeInsets.only(top: 16.0),
-            child: Text('Farm House Lembang')),
-      ],
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 16.0),
+              child: Text('Farm House Lembang'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
