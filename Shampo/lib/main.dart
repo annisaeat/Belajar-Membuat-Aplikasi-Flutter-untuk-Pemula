3be2084c069567709ca1d7011046f201a65ea55b
Annisa Eva <annisa.eat@gmail.com>
2021-07-28 23:01:57 +0700

    1

--- Shampo/lib/main.dart
+++ Shampo/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shampo',
+      title: 'Wisata Bandung',
       theme: ThemeData(),
       home: MainScreen(),
     );
